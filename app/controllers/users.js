975b3c1afc581b9cf30b22d2fd4071500476e4cf
ba11breaker <15212219417@163.com>
2020-07-24 20:48:04 +1000

    update

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -15,16 +15,42 @@ class UsersController {
     }
 
     create(ctx) {
+        ctx.verifyParams({
+            name: {
+                type: 'string',
+                required: true,
+            },
+            age: {
+                type: 'number',
+                required: false,
+            }
+        });
         db.push(ctx.request.body);
         ctx.body = ctx.request.body;
     }
 
     update(ctx) {
+        if(ctx.params.id * 1 >= db.length) {
+            ctx.throw(412);
+        }
+        ctx.verifyParams({
+            name: {
+                type: 'string',
+                required: true,
+            },
+            age: {
+                type: 'number',
+                required: false,
+            }
+        });
         db[ctx.params.id * 1] = ctx.request.body;
         ctx.body = ctx.request.body;
     }
 
     delete(ctx) {
+        if(ctx.params.id * 1 >= db.length) {
+            ctx.throw(412);
+        }
         db.splice(ctx.params.id * 1, 1);
         ctx.status = 204;
     }
--- app/index.js
+++ app/index.js
@@ -1,9 +1,10 @@
 const Koa = require('koa');
 const bodyparser = require('koa-bodyparser');
 const error = require('koa-json-error');
+const parameter = require('koa-parameter');
 const app = new Koa();
 const routing = require('./routes');
-
+// 7 - 3
 app.use(error({
     postFormat: (err, { stack, ...rest }) => {
         return process.env.NODE_ENV === 'production'
@@ -11,6 +12,7 @@ app.use(error({
     }
 }));
 app.use(bodyparser());
+app.use(parameter(app));
 routing(app);
 
 app.listen(3000, () => {
